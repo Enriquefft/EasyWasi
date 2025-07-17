e69ef49587a16222ea69918220cfe0cf884296f0
Enriquefft <enriquefft2001@gmail.com>
2025-07-17 17:14:26 -0500

    chore: regular commit

--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,6 @@
 import { vercel } from "@t3-oss/env-core/presets-zod";
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-import { getBaseUrl } from "./lib/utils.ts";
-
-const url = getBaseUrl();
 
 export const env = createEnv({
 	client: {
@@ -16,17 +13,11 @@ export const env = createEnv({
 	emptyStringAsUndefined: false,
 	extends: [vercel()],
 	runtimeEnv: {
-		BETTER_AUTH_URL: process.env["BETTER_AUTH_URL"],
 		DRIZZLE_DATABASE_URL: process.env["DRIZZLE_DATABASE_URL"],
-		GOOGLE_CLIENT_ID: process.env["GOOGLE_CLIENT_ID"],
-		GOOGLE_CLIENT_SECRET: process.env["GOOGLE_CLIENT_SECRET"],
 		NEXT_PUBLIC_APP_URL: process.env["NEXT_PUBLIC_APP_URL"],
 		NEXT_PUBLIC_PROJECT_NAME: process.env["NEXT_PUBLIC_PROJECT_NAME"],
 	},
 	server: {
-		BETTER_AUTH_URL: z.string().default(url),
 		DRIZZLE_DATABASE_URL: z.string().url(),
-		GOOGLE_CLIENT_ID: z.string(),
-		GOOGLE_CLIENT_SECRET: z.string(),
 	},
 });
